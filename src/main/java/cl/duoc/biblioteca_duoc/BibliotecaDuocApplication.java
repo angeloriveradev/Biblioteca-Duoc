157bdd1d1d6eeed1ef8267e978e761508ee9a782
VI-Alumno <VI-Alumno@duoc.acad>
2026-03-18 21:11:23 -0300

    Primer commit

---
+++ src/main/java/cl/duoc/biblioteca_duoc/BibliotecaDuocApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.biblioteca_duoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDuocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/biblioteca_duoc/BibliotecaDuocApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.biblioteca_duoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
